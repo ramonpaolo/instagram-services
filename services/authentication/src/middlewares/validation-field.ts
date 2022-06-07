c777116cef3ab81824d80289b87aeeb44b33a015
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-06-07 19:54:21 -0300

    feat(middlewares): added middlewares to verify fields to authentication user

--- services/authentication/src/middlewares/validation-field.ts
+++ services/authentication/src/middlewares/validation-field.ts
@@ -6,7 +6,7 @@ function validationName(req: Request, res: Response, next: NextFunction) {
     const name: string | null = req.body.name
 
     if (name === null) return returnError('field name equal null', res)
-    else if (name.length <= 3) return returnError('name is less than 3', res)
+    else if (name.length <= 4) return returnError('name is less than 4', res)
     else if (name.length >= 32) return returnError('name is more than 32', res)
     next()
 }
@@ -19,4 +19,23 @@ function validationTokenNotification(req: Request, res: Response, next: NextFunc
     next()
 }
 
-export { validationName, validationTokenNotification };
+function validationPassword(req: Request, res: Response, next: NextFunction) {
+    const password: string | null = req.body.password
+    
+    if (password === null) return returnError('field password equal null', res)
+    else if (password.length <= 11) return returnError('password is less than 11', res)
+    else if (password.length >= 42) return returnError('password is more than 42', res)
+    next()
+}
+
+function validationEmail(req: Request, res: Response, next: NextFunction) {
+    const email: string | null = req.body.email
+    
+    if (email === null) return returnError('field email equal null', res)
+    else if (email.length <= 20) return returnError('email is less than 20', res)
+    else if (email.length >= 42) return returnError('email is more than 42', res)
+    else if(!email.includes('@')) return returnError('not is email', res)
+    next()
+}
+
+export { validationName, validationTokenNotification, validationEmail, validationPassword };
