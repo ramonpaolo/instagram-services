133b652924560605539fe39cf5ac7a0d5cbd7f77
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-06-07 18:46:59 -0300

    test(tests): added tests with jest and supertest

--- services/authentication/jest.config.ts
+++ services/authentication/jest.config.ts
@@ -23,7 +23,7 @@ export default {
     // collectCoverageFrom: undefined,
   
     // The directory where Jest should output its coverage files
-    coverageDirectory: "coverage",
+    coverageDirectory: 'coverage',
   
     // An array of regexp pattern strings used to skip coverage collection
     // coveragePathIgnorePatterns: [
@@ -31,7 +31,7 @@ export default {
     // ],
   
     // Indicates which provider should be used to instrument code for coverage
-    coverageProvider: "v8",
+    coverageProvider: 'v8',
   
     // A list of reporter names that Jest uses when writing coverage reports
     // coverageReporters: [
@@ -72,12 +72,12 @@ export default {
   
     // An array of file extensions your modules use
     moduleFileExtensions: [
-      "js",
-      "jsx",
-      "ts",
-      "tsx",
-      "json",
-      "node"
+      'js',
+      'jsx',
+      'ts',
+      'tsx',
+      'json',
+      'node'
     ],
   
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
--- services/authentication/src/__tests__/index.spec.ts
+++ services/authentication/src/__tests__/index.spec.ts
@@ -1,60 +1,198 @@
+import { randomUUID } from 'crypto';
+import mongoose from 'mongoose';
 import request from 'supertest';
 
+// ExpressJs
 import app from '../index'
 
-describe('POST /user', () => {
-    const route = '/user'
+jest.setTimeout(60000)
 
-    jest.setTimeout(60000);
+const correctCredentials = {
+    name: 'User Test',
+    email: 'usertest32usertest@text.com',
+    password: 'user-test123',
+    'token-notification': 'qindfasfbsdifbsdfuhsvufhsvufvasfdbpiasvfivasdfvasfipd',
+    image: __dirname + '/image.jpg',
+    _id: 'usertest'
+}
 
-    afterAll(async () => {
-        app.close()
+const wrongCredentials = {
+    name: 'User',
+    email: 'usertest@text.com',
+    password: 'user-te',
+    'token-notification': 'dsffdgdfsjsdnfiasid',
+    image: __dirname + '/image.jpg',
+    _id: 'usertest'
+}
+
+describe('POST /user/register', () => {
+    // Delete the user before execute tests
+    beforeAll(async () => {
+        await request(app).delete('/user/' + correctCredentials._id);
     })
 
     it('response status has to be equal 200 where create user with success', async () => {
-        const response = await request(app).post(route).send({
-            name: 'asdadasd',
-            'token-notification': 'qindfasfbsdifbsdfuhsvufhsvufvasfdbpiasvfivasdfvasfipd'
-        })
+        const response = await request(app).post('/user/register').
+            field('name', correctCredentials.name).
+            field('email', correctCredentials.email).
+            field('password', correctCredentials.password).
+            field('token-notification', correctCredentials['token-notification']).
+            attach('image', correctCredentials.image)
 
-        // console.log(response)
+        expect(response.status).toBe(201)
+    })
 
-        expect(response.status).toBe(200)
+    it('return status 404 where user already exists', async () => {
+        const response = await request(app).post('/user/register').
+            field('name', correctCredentials.name).
+            field('email', correctCredentials.email).
+            field('password', correctCredentials.password).
+            field('token-notification', correctCredentials['token-notification']).
+            attach('image', correctCredentials.image)
+
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'user already exist' })
+
     })
 
     it('response status has to be equal 404 where name user is invalid', async () => {
-        const response = await request(app).post(route).send({
-            name: 'asd',
-            'token-notification': '92ncnsdfhbshfbsfasdfhubf453525325wdfsdfsdfgsdfgdfg'
-        })
+        const response = await request(app).post('/user/register').
+            field('name', wrongCredentials.name).
+            field('email', correctCredentials.email).
+            field('password', correctCredentials.password).
+            field('token-notification', correctCredentials['token-notification']).
+            attach('image', correctCredentials.image)
 
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'name is less than 4' })
+    })
+
+    it('response status has to be equal 404 where email user is invalid', async () => {
+        const response = await request(app).post('/user/register').
+            field('name', correctCredentials.name).
+            field('email', wrongCredentials.email).
+            field('password', correctCredentials.password).
+            field('token-notification', correctCredentials['token-notification']).
+            attach('image', correctCredentials.image)
+
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'email is less than 20' })
+    })
+
+    it('response status has to be equal 404 where password user is invalid', async () => {
+        const response = await request(app).post('/user/register').
+            field('name', correctCredentials.name).
+            field('email', correctCredentials.email).
+            field('password', wrongCredentials.password).
+            field('token-notification', correctCredentials['token-notification']).
+            attach('image', correctCredentials.image)
+
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'password is less than 11' })
+
+    })
+
+    it('response status has to be equal 404 where token-notification user is invalid', async () => {
+        const response = await request(app).post('/user/register').
+            field('name', correctCredentials.name).
+            field('email', correctCredentials.email).
+            field('password', correctCredentials.password).
+            field('token-notification', wrongCredentials['token-notification']).
+            attach('image', correctCredentials.image)
+
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'token-notification is less than 48' })
+    })
+})
+
+describe('POST /user/login', () => {
+    it('response status has to be equal 200 where user logged with success', async () => {
+        const response = await request(app).post('/user/login').send({
+            'token-notification': correctCredentials['token-notification'],
+            email: correctCredentials.email,
+            password: correctCredentials.password
+        })
+        expect(response.status).toBe(200)
+    })
+
+    it('response status has to be equal 401 where password user not is equal', async () => {
+        const response = await request(app).post('/user/login').send({
+            'token-notification': correctCredentials['token-notification'],
+            email: correctCredentials.email,
+            password: 'efgsfg1209dcs.ax'
+        })
+        expect(response.status).toBe(401)
+        expect(response.body).toEqual({ status: 'error', message: 'password is wrong' })
+    })
+
+    it('return error where try login with not existent user', async () => {
+        const response = await request(app).post('/user/login').send({
+            'token-notification': correctCredentials['token-notification'],
+            email: 'wrongCredentials.email@gmail.com',
+            password: correctCredentials.password
+        })
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'user not exists' })
+    })
+})
+
+describe('GET /user/:id', () => {
+    it('response status has to be equal 200 where get user with success', async () => {
+        const response = await request(app).get('/user/' + correctCredentials._id);
+        expect(response.status).toBe(200)
+    })
+
+    it('response status has to be equal 404 where user not exists', async () => {
+        const response = await request(app).get('/user/' + randomUUID());
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({ status: 'error', message: 'user not found' })
+    })
+})
+
+describe('PUT /user/:id', () => {
+    it('response status has to be equal 200 where updated user was a success', async () => {
+        const response = await request(app).put('/user/' + correctCredentials._id).send({
+            name: 'usertest2',
+            'token-notification': 'osdfnoasfdjbdsjifbisdbfibihjsdfbihjasbdfhuasbdufoasdnfosdnjfd'
+        })
+        expect(response.status).toBe(200)
+    })
+
+    it('response status has to be equal 404 where id user is null', async () => {
+        const response = await request(app).put('/user/');
         expect(response.status).toBe(404)
     })
 
-    it('response message has to be equal null where name user is null', async () => {
-        const response = await request(app).post(route).send({
-            name: '',
-            'token-notification': '92ncnsdfhbshfbsfasdfhubf453525325wdfsdfsdfgsdfgdfg'
+    it('response status has to be equal 404 where name user is invalid', async () => {
+        const response = await request(app).put('/user/' + correctCredentials._id).send({
+            name: wrongCredentials.name,
+            'token-notification': 'osdfnoasfdjbdsjifbisdbfibihjsdfbihjasbdfhuasbdufoasdnfosdnjfd'
         })
-
         expect(response.status).toBe(404)
     })
 
-    it('response status has to be equal 404 where token-notification is invalid', async () => {
-        const response = await request(app).post(route).send({
-            name: 'Jhon',
-            'token-notification': '4321'
+    it('response status has to be equal 404 where token-notification user is invalid', async () => {
+        const response = await request(app).put('/user/' + correctCredentials._id).send({
+            name: 'usertest2',
+            'token-notification': wrongCredentials['token-notification']
         })
-
-        expect(response.status).toBe(404)
-    })
-
-    it('response message has to be equal null where token-notification is null', async () => {
-        const response = await request(app).post(route).send({
-            name: 'Jhon',
-            'token-notification': ''
-        })
-
         expect(response.status).toBe(404)
     })
 })
+
+describe('DELETE /user/:id', () => {
+    it('response status has to be equal 200 where delete user with success', async () => {
+        const response = await request(app).delete('/user/' + correctCredentials._id);
+        expect(response.status).toBe(200)
+    })
+
+    it('response status has to be equal 404 where user not exists', async () => {
+        const response = await request(app).delete('/user/' + correctCredentials._id);
+        expect(response.status).toBe(400)
+    })
+})
+
+app.close();
+(async () => {
+    await mongoose.connection.close()
+})()
